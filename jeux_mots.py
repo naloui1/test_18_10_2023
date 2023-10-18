c76992c6e6e9c7758f103eb7ec211977e0b642df
naloui1 <nadia.aloui@yahoo.fr>
2023-10-18 09:23:54 +0200

    appel de la fonction derniere_consonne modifiee

--- jeux_mots.py
+++ jeux_mots.py
@@ -42,7 +42,7 @@ def derniere_consonne(mot):
             i=i-1
     return indice,mot[indice]
 
-print(derniere_consonne('arrivee'))
+print(derniere_consonne('depart'))
 #4)
 def double_consonne(mot):
     test=False
